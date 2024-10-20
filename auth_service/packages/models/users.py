cd0f0feb2ed807bbd93c3e2f9fb126cfbf20d604
Aleksei Astapov <Alexey.Astapoff@yandex.ru>
2024-10-20 14:50:26 +0300

    add two models for users

---
+++ auth_service/packages/models/users.py
@@ -0,0 +1,27 @@
+import pydantic
+import enum
+
+
+class UserRole(str, enum.Enum):
+    """Possible user roles"""
+
+    Admin = 'Admin'
+    DataScientist = 'DataScientist'
+    user = 'user'
+
+
+class User(pydantic.BaseModel):
+    """User model"""
+
+    email: pydantic.EmailStr
+    password: pydantic.SecretStr
+    role: UserRole
+
+    class Config:
+        schema_extra = {
+            'example': {
+                'email': 'uVHrN@example.com',
+                'password': 'password',
+                'roll': 'DataScientist'
+            },
+        }
