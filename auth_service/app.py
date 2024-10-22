707ade1bb54ad6f57c2d228d6cdced0b5a4c054d
Aleksei Astapov <Alexey.Astapoff@yandex.ru>
2024-10-22 09:10:59 +0300

    Making edits after review

--- auth_service/app.py
+++ auth_service/app.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI, status
+
+from auth_service.packages.config.constants import MAIN_HOST, MAIN_PORT
 
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World!"}
+@app.get("/health", status_code=status.HTTP_200_OK)
+async def healthcheck():
+    return {'status': 'ok'}
+
+
+def main():
+    uvicorn.run('app:app', host=MAIN_HOST, port=MAIN_PORT)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    main()
