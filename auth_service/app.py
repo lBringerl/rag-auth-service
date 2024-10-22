85fa5c666b5c8c916c108144072e89cde5f29d14
Aleksei Astapov <Alexey.Astapoff@yandex.ru>
2024-10-22 14:54:45 +0300

    fix folder structure and add JSONResponse

--- auth_service/app.py
+++ auth_service/app.py
@@ -1,20 +1,23 @@
 import uvicorn
 from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 
-from auth_service.packages.config.constants import MAIN_HOST, MAIN_PORT
+from auth_service.config.constants import MAIN_HOST, MAIN_PORT
 
 
 app = FastAPI()
 
 
-@app.get("/health", status_code=status.HTTP_200_OK)
+@app.get('/health', status_code=status.HTTP_200_OK)
 async def healthcheck():
-    return {'status': 'ok'}
+    return JSONResponse(
+        content={'status': 'OK'}, status_code=status.HTTP_200_OK
+    )
 
 
 def main():
     uvicorn.run('app:app', host=MAIN_HOST, port=MAIN_PORT)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
